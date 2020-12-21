a80b889b06cbeebf45cec1987c5d184b79242dec
Tizoc <jcondonpro@gmail.com>
2020-12-21 13:22:50 -0800

    script.js basic structure

---
+++ script.js
@@ -0,0 +1,25 @@
+var days = {
+sunday ="#sunday",
+monday ="#monday",
+tuesday ="#tuesday",
+wednesday ="#wednesday",
+thursday ="#thursday",
+friday ="#friday",
+saturday ="#saturday",
+
+}
+
+var calendar = "#mainDivBox"
+var button = "#btn"
+var input = "#input"
+var textarea = "#textarea"
+
+days.button.on("click", function(){
+    
+})
+days.input.on("click", function(){
+
+})
+days.textarea.on("click", function(){
+
+})
