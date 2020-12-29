73144756ace858b4af419c3a497d93f757977bcc
Drantho <drantho@gmail.com>
2020-12-28 19:20:00 -0800

    event functions

--- script.js
+++ script.js
@@ -3,14 +3,6 @@
 // =============================================================================
 
 // jQuery element varaibles
-var sunday = "#sunday";
-var monday = "#monday";
-var tuesday = "#tuesday";
-var wednesday = "#wednesday";
-var thursday = "#thursday";
-var friday = "#friday";
-var saturday = "#saturday";
-
 var week = $("#week")
 var save = $(".save")
 var bored = $(".bored")
@@ -32,17 +24,6 @@ var currentDate = new Date();
 
 // user's master event list from localStorage
 var eventsList;
-
-function weekViewElement(eventObj){
-
-    var event = $("<div>");
-    event.addClass();
-
-
-
-
-}
-
 
 // =====================================================================================
 // Page setup
@@ -75,8 +56,8 @@ function fillMonth() {
     monthTitle.text(`${getMonthName(monthViewed)} ${yearViewed}`);
 
     // put day number on each day of the month
-    for(var i=0; i<getDaysInMonth(currentDate.getUTCMonth()+1, currentDate.getUTCFullYear()); i++){
-        $("#monthDay" + (i+getFirstDayOfMonth())).text(i+1)
+    for (var i = 0; i < getDaysInMonth(currentDate.getUTCMonth() + 1, currentDate.getUTCFullYear()); i++) {
+        $("#monthDay" + (i + getFirstDayOfMonth())).text(i + 1)
     }
 
     // loop through each event
@@ -84,19 +65,10 @@ function fillMonth() {
 
         var tempDate = new Date(eventsList[i].eventDate)
 
-        if(eventsList[i].eventDescription == "Christmas Eve!"){
-            console.log("==========================================");            
-            
-            console.log((tempDate.getUTCMonth() + 1) === monthViewed);
-            console.log(tempDate.getFullYear() == yearViewed);
-
-            console.log("==========================================");            
-        }
-        
         // if event belongs in current month - display on month grid
         if (tempDate.getUTCMonth() + 1 === monthViewed && tempDate.getFullYear() == yearViewed) {
-            
-            $("#monthDay" + (tempDate.getDate()+1)).append(eventsList[i].eventDescription);
+
+            $("#monthDay" + (tempDate.getDate() + 1)).append(eventsList[i].eventDescription);
         }
     }
 
@@ -107,22 +79,17 @@ fillMonth();
 
 // fill week view with current week data from eventList
 function fillWeek() {
-    console.log("fillWeek() fires")
-    
-    for(var i=0; i<eventsList.length; i++){
+    // console.log("fillWeek() fires")
+
+    for (var i = 0; i < eventsList.length; i++) {
         var eventDate = new Date(eventsList[i].eventDate);
 
-        console.log("============================================================");
-        console.log(`${eventDate.getUTCMonth()+1} === ${monthViewed}`);
-        console.log(`${eventDate.getUTCFullYear()} === ${eventDate.getUTCFullYear()}`);
-        console.log(`${getWeekNumber(eventDate)} === ${weekViewed}`);
-        console.log("============================================================");
-
-        if(eventDate.getUTCMonth()+1 === monthViewed && eventDate.getUTCFullYear() === eventDate.getUTCFullYear() && getWeekNumber(eventDate) === weekViewed){
-            $("#weekDay" + (((getFirstDayOfMonth(eventDate) + eventDate.getUTCDate()) % 7)-1) ).append(eventsList[i].eventDescription)            
+        if (eventDate.getUTCMonth() + 1 === monthViewed && eventDate.getUTCFullYear() === eventDate.getUTCFullYear() && getWeekNumber(eventDate) === weekViewed) {
+            var weekDay = $("#weekDay" + (((getFirstDayOfMonth(eventDate) + eventDate.getUTCDate()) % 7) - 1));
+            weekDay.append(weekViewElement(eventsList[i]));
         }
     }
-    
+
     weekTitle.text(`${getMonthName(monthViewed)} ${yearViewed} Week ${weekViewed}`);
 }
 
@@ -130,13 +97,23 @@ function fillWeek() {
 fillWeek();
 
 // fill day view with current day data from eventList
-function fillDay(){
+function fillDay() {
     // TODO
-} 
+}
 
 // call on page load
 fillDay();
 
+function weekViewElement(eventObj) {
+
+    var event = $("<div>");
+    event.addClass("event");
+
+    event.css("height", getPercentOfDay(getDuration(eventObj)) + "%");
+    event.css("top", getPercentOfDay(convertHours(eventObj.startTime)) + "%");
+
+    return event;
+}
 
 // =====================================================================================
 // Page functions
@@ -161,7 +138,7 @@ $("#addEventBtn").click(function () {
         eventDescription: eventDescription.val()
     }
 
-    
+
     // console.log("addEventButton() fires ", eventObj)
 
     eventsList.push(eventObj);
@@ -204,7 +181,7 @@ function setQuote(textObj) {
 // needed to position all days in month view
 function getFirstDayOfMonth(date = currentDate) {
     var year = date.getFullYear();
-    var day = new Date(year + "-" + monthViewed + "-01").getDay()+1;
+    var day = new Date(year + "-" + monthViewed + "-01").getDay() + 1;
 
     // console.log(`add ${day} days to first of month`);
 
@@ -212,25 +189,25 @@ function getFirstDayOfMonth(date = currentDate) {
 }
 
 // returns number of days in selected month
-function getDaysInMonth (month, year) {
+function getDaysInMonth(month, year) {
     return new Date(year, month, 0).getDate();
 }
 
-function getMonthName(num){
+function getMonthName(num) {
     console.log(num)
-    switch(num){
+    switch (num) {
         case 1:
-            return "January";            
+            return "January";
         case 2:
             return "February";
         case 3:
             return "March";
         case 4:
-            return "April";    
+            return "April";
         case 5:
-            return "May";        
+            return "May";
         case 6:
-            return "June";        
+            return "June";
         case 7:
             return "July";
         case 8:
@@ -240,7 +217,7 @@ function getMonthName(num){
         case 10:
             return "October";
         case 11:
-            return "November";  
+            return "November";
         case 12:
             return "December";
     }
@@ -248,32 +225,42 @@ function getMonthName(num){
 }
 
 // returns current week number 0-4
-function getWeekNumber(date = currentDate){
+function getWeekNumber(date = currentDate) {
     date = new Date(date);
     var cellNumber = getFirstDayOfMonth() - 1 + date.getUTCDate();
-    console.log(`getFirstDayOfMonth() + date.getUTCDate()  : ${getFirstDayOfMonth()} + ${date.getUTCDate()}`);
-
-    console.log("cellNumber: ", cellNumber);
 
     var weekNumber = Math.floor(cellNumber / 7);
-
-    console.log("weekNumber: ", weekNumber);
 
     return weekNumber;
 }
 
-getWeekNumber();
+function getDuration(eventObj) {
 
-function getDuration(eventObj){
-    var startTime = eventObj.startTime.replace(" ", ":");
-    var endTime = eventObj.endTime.replace(" ", ":");
+    return convertHours(eventObj.endTime) - convertHours(eventObj.startTime)
 
-    var startTimeArr = startTime.split(":");
-    var endTimeArr = endTime.split(":");
-
-    
 }
 
+function convertHours(str) {
+    var intTime = 0;
+
+    var time = str.replace(" ", ":");
+
+    var timeArr = time.split(":");
+
+    intTime += parseInt(timeArr[0]);
+
+    intTime += parseInt(timeArr[1]) / 60;
+
+    if (timeArr[2] === "PM") {
+        intTime += 12;
+    }
+
+    return intTime;
+}
+
+function getPercentOfDay(num){
+    return (num/24) * 100;
+}
 
 // ===================================================================================
 // Old functions
@@ -304,3 +291,11 @@ function getDuration(eventObj){
 //     // store them both to local storage
 //     localStorage.setItem(thisDay, dayText)
 // })
+
+// var sunday = "#sunday";
+// var monday = "#monday";
+// var tuesday = "#tuesday";
+// var wednesday = "#wednesday";
+// var thursday = "#thursday";
+// var friday = "#friday";
+// var saturday = "#saturday";
