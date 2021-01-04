f3303b89a1f0fb857e5e4d5386eb43c470b9d192
Tizoc <jcondonpro@gmail.com>
2021-01-04 10:44:55 -0800

    move to branch

--- api.js
+++ api.js
@@ -1,20 +1,19 @@
 // console.log("api.js loaded");
-var boredUrl = "http://www.boredapi.com/api/activity/?type=";
-var quoteUrl = "https://type.fit/api/quotes";
 
 
 // get activity from bored api - call data setting callback function
 function getRandomBoredEventApi(callback, type = ""){
-
+    
+    var boredUrl = "http://www.boredapi.com/api/activity/?type=";
     boredUrl += type;
-
+    
     // async call
     $.ajax({
         url: boredUrl,
         method: "GET"
     }).then(function(response){
         console.log("bored api event: ", response);
-
+        
         // call display setting function
         callback(response);
     }).fail(function(){
@@ -24,24 +23,25 @@ function getRandomBoredEventApi(callback, type = ""){
             participants: "1",
             type: "diy"
         }
-
+        
         // call display setting function
         callback(activity);
     })
-
+    
 }
 
 // get quote from type.fit api
 function getQuoteApi(callback){
-
+    var quoteUrl = "https://type.fit/api/quotes";
+    
     // async call
     $.ajax({
         url: quoteUrl,
         method: "GET"
-        }
+    }
     ).then(function(response){
         // console.log(response)
-
+        
         // convert response to object
         var quotes=JSON.parse(response);
         
