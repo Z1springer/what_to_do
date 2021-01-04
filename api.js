f2ca18522d54ef175a52801762c9deab8f4dd8cb
Drantho <drantho@gmail.com>
2021-01-04 00:01:38 -0800

    code cleaned, commented

--- api.js
+++ api.js
@@ -1,66 +1,68 @@
-console.log("api.js loaded");
-
-function getQuote(callback){
-
-    var quote = "";
+// console.log("api.js loaded");
+var boredUrl = "http://www.boredapi.com/api/activity/?type=";
+var quoteUrl = "https://type.fit/api/quotes";
 
 
-    // dummy call
-    quote = "Speak softly and carry a big stick"
-
-    callback(quote);
-}
-
-function getRandomBoredEvent(callback, type = "education"){
-
-    var bored = {};
-    
-    // dummy call
-    bored = {
-        "activity": "Learn a new programming language",
-        "accessibility": 0.25,
-        "type": type,
-        "participants": 1,
-        "price": 0.1,
-        "key": "5881028"
-    };
-
-    callback(bored);
-}
-
+// get activity from bored api - call data setting callback function
 function getRandomBoredEventApi(callback, type = ""){
 
-    var url = "http://www.boredapi.com/api/activity/?type=" + type;
-    
+    boredUrl += type;
+
+    // async call
     $.ajax({
-        url: url,
+        url: boredUrl,
         method: "GET"
     }).then(function(response){
         console.log("bored api event: ", response);
+
+        // call display setting function
         callback(response);
+    }).fail(function(){
+        // if unable to get activity, send fix connection as activity
+        var activity = {
+            activity: "Fix your internet connection!",
+            participants: "1",
+            type: "diy"
+        }
+
+        // call display setting function
+        callback(activity);
     })
 
 }
 
-
+// get quote from type.fit api
 function getQuoteApi(callback){
 
+    // async call
     $.ajax({
-        url: "https://type.fit/api/quotes",
+        url: quoteUrl,
         method: "GET"
         }
     ).then(function(response){
         // console.log(response)
+
+        // convert response to object
         var quotes=JSON.parse(response);
         
+        // get random quote from response
         var quote = quotes[Math.floor(Math.random()*quotes.length)];
 
+        // if author is blank, attribute to Unknown
         if(!quote.author){
             quote.author = "Unknown";
         }
 
-        // console.log(quote);
+        // call quote display function
         callback(quote);
+
+    }).fail(function(){
+
+        // if unable to get quote, send snarky quote
+        callback({
+            text: "Fix your internet connection!",
+            author: "Abraham Lincoln"
+        })
     });
 
 }
